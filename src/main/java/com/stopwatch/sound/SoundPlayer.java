7ab4a8cdd91b560856e70f72c6a2cb3f72db9a50
Filip Dobrosavljevic <filipdobro1999@gmail.com>
2026-02-22 17:28:08 +0100

    fix: Removed specifying exceptions

--- src/main/java/com/stopwatch/sound/SoundPlayer.java
+++ src/main/java/com/stopwatch/sound/SoundPlayer.java
@@ -3,10 +3,6 @@ package com.stopwatch.sound;
 import com.stopwatch.StopWatchConfig;
 import net.runelite.client.audio.AudioPlayer;
 
-import java.io.IOException;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
 import lombok.extern.slf4j.Slf4j;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -25,7 +21,7 @@ public class SoundPlayer {
         float gain = 20f * (float) Math.log10(config.alertVolume() / 100f);
         try {
             audioPlayer.play(SoundPlayer.class, "/sound/alert.wav", gain);
-        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+        } catch (Exception e) {
             log.warn("Failed to play StopWatch alert", e);
         }
     }
